9a113e7856d1949086f5b93053ac10ca705e5fd0
Rodrigo-Santoos <rodrigoyukimide@gmail.com>
2021-04-14 14:25:25 -0300

    Feito na plataforma Unity

---
+++ PrimeiroGame/Assets/Game/Scripts/obstacles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacles : MonoBehaviour
+{
+    private Rigidbody2D objRB;
+    void Start()
+    {
+        objRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objRB.velocity = new Vector2(-5f,0f);
+    }
+}
